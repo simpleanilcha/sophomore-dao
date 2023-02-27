7de0a936fc06b567b1a2cfe9d665ba470d02c2da
Anil Maharjan <simpleanilcha@gmail.com>
2023-02-27 17:06:15 +0545

    build: on-chain dao

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x521737B8c5C113aF7D25ce070a4BDAcC47EBEc55";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
